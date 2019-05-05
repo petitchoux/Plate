3c9cf50f7e91f24bc146f5501b240427068ada79
Joyce Huang <joycalation@gmail.com>
2019-05-05 17:54:26 -0400

    Debugged JS. Updated About, FAQ, and Credits

--- sessionStorage.js
+++ sessionStorage.js
@@ -1,20 +1,34 @@
-var like_button = document.getElementById('swipeRight')
-like_button.onclick = SaveData;
+function getSavedImages() {
+    var prevState = localStorage.getItem('imgUrls');
+    var matches = (prevState == null || prevState == "") ? [] : prevState.split(",");
+    return matches
+}
 
 function SaveData() {
-    var matches = [];
-    var input = document.getElementById('image').src;
-    matches = JSON.parse(sessionStorage.getItem('image'));
-    matches.push(data);
-    alert(matches);
-    sessionStorage.setItem('image', JSON.stringify(matches));
+    let matches = getSavedImages();
+    var activeImgSrc = getActiveImageUrl();
+    matches.push(activeImgSrc);
+    localStorage.setItem('imgUrls', matches);
+}
+
+function addImageToDocumentMatch(imgUrl) {
+    var node = document.createElement("LI");
+    var newImage = document.createElement("IMG");
+    newImage.setAttribute("src", imgUrl);
+    node.appendChild(newImage);
+    document.getElementById("matches").appendChild(node);
 }
 
 function displayData() {
-    var retrievedData = localStorage.getItem("image");
-    var list2 = JSON.parse(retrievedData);
-    alert(list2.length);
-    for (i = 0; i < matches.length; i++) {
-        document.write("<li><img src='" + matches[i] + "/></li>");
+    var prevState = localStorage.getItem('imgUrls');
+    var matches = (prevState == null || prevState == "") ? [] : prevState.split(",");
+    for (let match of matches) {
+        addImageToDocumentMatch(match);
     }
+}
+
+function getActiveImageUrl() {
+    let activeDiv = document.getElementsByClassName("fotorama__stage__frame fotorama__loaded fotorama__loaded--img fotorama__active")[0];
+    let imgSrc = activeDiv.getElementsByTagName("img")[0].src;
+    return imgSrc
 }
